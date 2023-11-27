7f7ef9a94fdbf064061e83811ffaf4d7a8aa5bb6
novaotp <rahmansajidur30@gmail.com>
2023-11-27 12:53:28 +0100

    Fixed script.sql and removed backup.sql

--- src/database/scripts/script.sql
+++ src/database/scripts/script.sql
@@ -1,30 +1,33 @@
 -- Recreates the database
-CREATE TABLE user (
-  id SERIAL NOT NULL PRIMARY KEY,
+DROP SCHEMA IF EXISTS openrecipydb;
+CREATE SCHEMA openrecipydb;
+
+CREATE TABLE openrecipydb.user (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(80) NOT NULL,
-  password VARCHAR NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE recipy (
-  id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE openrecipydb.recipy (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   author_id INTEGER NOT NULL,
   name VARCHAR(80) NOT NULL,
   time INTEGER NOT NULL,
-  text VARCHAR NOT NULL,
-  CONSTRAINT fk_recipy_user FOREIGN KEY (author_id) REFERENCES user (id)
+  text TEXT NOT NULL,
+  CONSTRAINT fk_recipy_user FOREIGN KEY (author_id) REFERENCES openrecipydb.user (id)
 );
 
-CREATE TABLE ingredient (
-  id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE openrecipydb.ingredient (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE recipy_ingredient (
-  id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE openrecipydb.recipy_ingredient (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   recipy_id INTEGER NOT NULL,
   ingredient_id INTEGER NOT NULL,
-  CONSTRAINT fk_recipy_ingredient_recipy FOREIGN KEY (recipy_id) REFERENCES recipy (id),
-  CONSTRAINT fk_recipy_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient (id)
+  CONSTRAINT fk_recipy_ingredient_recipy FOREIGN KEY (recipy_id) REFERENCES openrecipydb.recipy (id),
+  CONSTRAINT fk_recipy_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES openrecipydb.ingredient (id)
 );
